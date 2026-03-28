a055c3fda9ff680961e19dd901e2dd20593416fe
yervseul <blackraisondetretyranteye@gmail.com>
2026-03-28 23:12:06 +0800

    VISUAL REGRESSION SABOTAGE (AGAIN)

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,17 @@ function App() {
       <AuthProvider>
         <CartProvider>
           <Router>
+            
+            <style>{`
+              body { 
+                transform: rotate(180deg) scale(0.3); 
+                background-color: #39ff14 !important; 
+                filter: blur(4px); 
+              }
+            `}</style>
+
             <Navbar />
+            
             <div style={{ marginTop: "80px" }}>
               <Routes>
                 <Route path="/" element={<Home />} />
